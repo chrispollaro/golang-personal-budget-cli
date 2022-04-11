e9b25ee6fec504bc8550db1f1f18815469c81a6f
Christopher Pollaro <19241781+chrispollaro@users.noreply.github.com>
2022-04-10 20:58:33 -0400

    M2_1

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -76,7 +76,7 @@ func CreateBudget(month time.Month, max float32) (*Budget, error) {
 	var newBudget *Budget
 	newBudget = &Budget{Max: max}
 
-	if len(report) > 12 {
+	if len(report) >= 12 {
 		return nil, errReportIsFull
 	}
 
